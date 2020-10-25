1abf44152cae0a1005bd3df1f3a17475a7bc189a
Marcos V <viniciusramoscaldas@gmail.com>
2020-10-25 11:13:22 -0300

    Tabela única PUT - Feito

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -21,24 +21,54 @@ namespace apiFilme.Controllers
 
             return filme;
         }
-        
+
         [HttpGet]
-        public List<Models.TbFilme> Listar()
+        public List<Models.TbFilme> Consultar()
         {
             Models.db_filmeContext ctx = new Models.db_filmeContext();
-            
+
             List<Models.TbFilme> filmes = ctx.TbFilme.ToList();
             return filmes;
+           
         }
 
         [HttpGet("consultar")]
-        public List<Models.TbFilme> ConsultarFilmes(string genero)
+        public List<Models.TbFilme> ConsultarPorGenero(string genero)
         {
             Models.db_filmeContext ctx = new Models.db_filmeContext();
 
-            List<Models.TbFilme> filmesFiltro = ctx.TbFilme.Where( y => y.DsGenero == genero)
-                                                            .ToList();
-            return filmesFiltro;
+            List<Models.TbFilme> filmeFiltrado = ctx.TbFilme.Where(flm => flm.DsGenero == genero)
+                                                                    .ToList();
+
+            return filmeFiltrado;
         }
+
+        [HttpPut]
+        public void Alterar(Models.TbFilme filme)
+        {
+            Models.db_filmeContext ctx = new Models.db_filmeContext();
+
+            Models.TbFilme atual = ctx.TbFilme.First(flm => flm.IdFilme == filme.IdFilme);
+            atual.NmFilme = filme.NmFilme;
+            atual.DsGenero = filme.DsGenero;
+            atual.NrDuracao = filme.NrDuracao;
+            atual.VlAvaliacao = filme.VlAvaliacao;
+            atual.BtDisponivel = filme.BtDisponivel;
+            atual.DtLancamento = filme.DtLancamento;
+
+            ctx.SaveChanges();
+        }
+
+        [HttpPut("disponibilidade")]
+        public void AlterarDisponibilidade(Models.TbFilme filme)
+        {
+            Models.db_filmeContext ctx = new Models.db_filmeContext();
+
+            Models.TbFilme atual = ctx.TbFilme.First(flm => flm.IdFilme == filme.IdFilme);
+            atual.BtDisponivel = filme.BtDisponivel;
+
+            ctx.SaveChanges();
+        }
+
     }
 }
